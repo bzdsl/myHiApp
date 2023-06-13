9aa341b5dda99e625856997ed83b0eacbef2e83f
Thang <1851060093@e.tlu.edu.vn>
2023-06-13 21:09:31 +0700

    updated

---
+++ app/src/main/java/com/example/myhiapp/activities/UsersActivity.java
@@ -0,0 +1,80 @@
+package com.example.myhiapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import com.example.myhiapp.adapters.UsersAdapter;
+import com.example.myhiapp.models.User;
+import com.example.myhiapp.databinding.ActivityUsersBinding;
+import com.example.myhiapp.utilities.Constants;
+import com.example.myhiapp.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersActivity extends AppCompatActivity {
+    private ActivityUsersBinding binding;
+    private PreferenceManager preferenceManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUsersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        setListeners();
+        getUser();
+    }
+
+    private void setListeners() {
+        binding.imageBack.setOnClickListener(v -> onBackPressed());
+    }
+    private void getUser() {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .get()
+                .addOnCompleteListener(task -> {
+                    loading(false);
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                        List<User> users = new ArrayList<>();
+                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                            if (currentUserId.equals(queryDocumentSnapshot.getId())) {
+                                continue;
+                            }
+                            User user = new User();
+                            user.name = queryDocumentSnapshot.getString(Constants.KEY_NAME);
+                            user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
+                            user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            users.add(user);
+                        }
+                        if (users.size() > 0) {
+                            UsersAdapter usersAdapter = new UsersAdapter(users);
+                            binding.userRecyclerView.setAdapter(usersAdapter);
+                            binding.userRecyclerView.setVisibility(View.VISIBLE);
+                        } else {
+                            showErrorMessage();
+                        }
+                    } else {
+                        showErrorMessage();
+                    }
+                });
+    }
+
+
+    private void showErrorMessage() {
+        binding.textErrorMessage.setText(String.format("%s","Không có ai hoạt động"));
+        binding.textErrorMessage.setVisibility(View.VISIBLE);
+    }
+
+    private void loading(Boolean isLoading) {
+        if(isLoading) {
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+}
